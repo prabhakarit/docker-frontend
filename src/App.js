d306558d9e09dff1a956e68c3c60b86f6398a329
Prabhakar borah <prabhakarborah@Prabhakars-MacBook-Pro.local>
2020-08-02 00:37:17 +0530

    added for docker ci/cd with travis and aws

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prabhakar is enjoying learning docker
         </p>
         <a
           className="App-link"
